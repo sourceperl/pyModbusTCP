4bc72bb8ac896decbfb90c0b7e323a9b71a47962
sourceperl <loic.celine@free.fr>
2014-08-05 19:06:09 +0200

    release v0.0.3

--- pyModbusTCP/const.py
+++ pyModbusTCP/const.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-VERSION                                     = "0.0.2"
+VERSION                                     = "0.0.3"
 ## ModBus/TCP
 MODBUS_PORT                                 = 502
 ## ModBus RTU
