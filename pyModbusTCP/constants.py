963a7f806ff8188543c47e1622b41ef60c18fd1b
sourceperl <loic.celine@free.fr>
2015-03-27 08:20:55 +0100

    release v0.0.11

--- pyModbusTCP/constants.py
+++ pyModbusTCP/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-VERSION                                     = "0.0.10"
+VERSION                                     = "0.0.11"
 ## ModBus/TCP
 MODBUS_PORT                                 = 502
 ## Modbus mode
