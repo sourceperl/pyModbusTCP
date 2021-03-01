9577e15fab519ea866285852da5818655f65bf34
sourceperl <loic.celine@free.fr>
2021-03-01 19:39:09 +0100

    improve __init__.py

--- pyModbusTCP/__init__.py
+++ pyModbusTCP/__init__.py
@@ -11,5 +11,12 @@
 #                 15
 #        Charset: utf-8
 
+from .constants import VERSION
+
+
 __all__ = ['constants', 'client', 'server', 'utils']
-from .constants import VERSION as __version__
+__title__ = 'pyModbusTCP'
+__description__ = 'A simple Modbus/TCP library for Python.'
+__url__ = 'https://github.com/sourceperl/pyModbusTCP'
+__version__ = VERSION
+__license__ = 'MIT'
