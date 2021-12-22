815a39ce648875d1df0b1d460868c214fe98046d
Andrii Tkachenko <atkachenko@maxcrc.de>
2021-12-22 10:32:48 +0100

    _recv_mbus updated. RTU mode correct receive for functions 0x05,0x06,0x10

--- pyModbusTCP/client.py
+++ pyModbusTCP/client.py
@@ -934,8 +934,10 @@ class ModbusClient:
             if not rx_header:
                 return None
             rx_header_array = bytearray(rx_header)
-            if rx_header_array[1] in [0x01,0x02,0x03,0x04]:
+            if rx_header_array[1] in [0x01, 0x02, 0x03, 0x04]:
                 rx_buffer = rx_header + self._recv_all(rx_header_array[2] + 2)
+            elif rx_header_array[1] in [0x05, 0x06, 0x10]:
+                rx_buffer = rx_header + self._recv_all(3 + 2)
             else:
                 # receive modbus RTU frame (max size is 256 bytes)
                 rx_buffer = rx_header + self._recv(253)
