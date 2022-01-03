1d124d497d21d49cee0975c6e10fe33caa7b7b1f
sourceperl <loic.celine@free.fr>
2022-01-03 01:31:56 +0100

    minor fixes

--- examples/min_read_bit.py
+++ examples/min_read_bit.py
@@ -2,4 +2,4 @@
 from pyModbusTCP.client import ModbusClient
 
 # minimal code: read 3 coils at @1000 on localhost server, print result
-print('coils=%s' % ModbusClient(auto_open=True).read_coils(1000,3))
+print('coils=%s' % ModbusClient(auto_open=True).read_coils(1000, 3))
