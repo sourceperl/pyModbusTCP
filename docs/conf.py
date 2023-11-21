258006433d00e9f0a7d2c8ca2430ae8b5999a08f
sourceperl <3809271+sourceperl@users.noreply.github.com>
2023-11-21 15:34:24 +0100

    update docs html theme

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pyModbusTCP'
-copyright = u'2022, Loïc Lefebvre'
+copyright = u'2023, Loïc Lefebvre'
 author = u'Loïc Lefebvre'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -124,7 +124,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
